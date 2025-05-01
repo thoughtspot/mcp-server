67b348a371375322fcfb6cb0c7acb66f9a468842
Ashish Shubham <ashish.shubham@thoughtspot.com>
2025-05-01 10:11:22 -0700

    Move to Low level server definition

--- src/index.ts
+++ src/index.ts
@@ -1,59 +1,114 @@
 import OAuthProvider from "@cloudflare/workers-oauth-provider";
 import { McpAgent } from "agents/mcp";
-import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
+import { Server } from "@modelcontextprotocol/sdk/server/index.js";
+import { CallToolRequestSchema, ListToolsRequestSchema, ToolSchema, Tool } from "@modelcontextprotocol/sdk/types.js";
 import { z } from "zod";
+import { zodToJsonSchema } from "zod-to-json-schema";
 import handler from "./handlers";
 import { Props } from "./utils";
 import { getThoughtSpotClient } from "./thoughtspot/thoughtspot-client";
 import { getRelevantData } from "./thoughtspot/relevant-data";
 
+
+const ToolInputSchema = ToolSchema.shape.inputSchema;
+type ToolInput = z.infer<typeof ToolInputSchema>;
+
+const PingSchema = z.object({});
+
+const GetRelevantDataSchema = z.object({
+    query: z.string().describe("The query to get relevant data for, this could be a high level task or question the user is asking or hoping to get answered")
+});
+
+enum ToolName {
+    Ping = "ping",
+    GetRelevantData = "getRelevantData",
+}
+
 export class ThoughtSpotMCP extends McpAgent<Env, any, Props> {
-    server = new McpServer({
+    server = new Server({
         name: "ThoughtSpot",
         version: "1.0.0",
+        capabilities: {
+            tools: {},
+            logging: {},
+            completion: {},
+            resources: {},
+        }
     });
 
     async init() {
-        this.server.tool("add", { a: z.number(), b: z.number() }, async ({ a, b }) => ({
-            content: [{ type: "text", text: String(a + b) }],
-        }));
-
-        this.server.tool(
-            "getRelevantData",
-            "Get relevant data from ThoughtSpot database",
-            {
-                query: z.string().describe("The query to get relevant data for")
-            },
-            async ({ query }) => {
-                const client = getThoughtSpotClient(this.props.instanceUrl, this.props.accessToken);
-                const relevantData = await getRelevantData(query, false, (data) => this.server.server.notification({
-                    method: "notifications/progress",
-                    params: {
-                        message: data,
-                        progressToken: Math.random().toString(36).substring(2, 15),
-                        progress: 50,
-                        total: 100,
+        // Handle list tools request
+        this.server.setRequestHandler(ListToolsRequestSchema, async () => {
+            return {
+                tools: [
+                    {
+                        name: ToolName.Ping,
+                        description: "Simple ping tool to test connectivity and Auth",
+                        inputSchema: zodToJsonSchema(PingSchema) as ToolInput,
                     },
-                }), client);
-                return {
-                    content: [{
-                        type: "text",
-                        text: relevantData.allAnswers.map((answer) => `Question: ${answer.question}\nAnswer: ${answer.data}`).join("\n\n")
-                    }, {
-                        type: "text",
-                        text: `Dashboard Url: ${relevantData.liveboard}`,
-                    }],
-                };
-            });
+                    {
+                        name: ToolName.GetRelevantData,
+                        description: "Get relevant data from ThoughtSpot database",
+                        inputSchema: zodToJsonSchema(GetRelevantDataSchema) as ToolInput,
+                    }
+                ]
+            };
+        });
 
-        // Tool that returns the user's bearer token
-        // This is just for demonstration purposes, don't actually create a tool that does this!
-        this.server.tool("getToken", {}, async () => ({
-            content: [{ type: "text", text: String(`User's token: ${this.props.accessToken}`) }],
-        }));
+        // Handle call tool request
+        this.server.setRequestHandler(CallToolRequestSchema, async (request: z.infer<typeof CallToolRequestSchema>) => {
+            const { name } = request.params;
+
+            switch (name) {
+                case ToolName.Ping:
+                    if (this.props.accessToken && this.props.instanceUrl) {
+                        return {
+                            content: [{ type: "text", text: "Pong" }],
+                        };
+                    } else {
+                        return {
+                            content: [{ type: "text", text: "ERROR: Not authenticated" }],
+                        };
+                    }
+
+                case ToolName.GetRelevantData: {
+                    return this.callGetRelevantData(request);
+                }
+
+                default:
+                    throw new Error(`Unknown tool: ${name}`);
+            }
+        });
     }
-}
 
+    async callGetRelevantData(request: z.infer<typeof CallToolRequestSchema>) {
+        const { query } = GetRelevantDataSchema.parse(request.params.arguments);
+        const client = getThoughtSpotClient(this.props.instanceUrl, this.props.accessToken);
+        const progressToken = request.params._meta?.progressToken;
+        let progress = 0;
+
+        const relevantData = await getRelevantData(query, false, (data) => this.server.notification({
+            method: "notifications/progress",
+            params: {
+                message: data,
+                progressToken: progressToken,
+                progress: Math.max(progress++ * 10, 100),
+                total: 100,
+            },
+        }), client);
+
+        return {
+            content: [{
+                type: "text",
+                text: relevantData.allAnswers.map((answer) => `Question: ${answer.question}\nAnswer: ${answer.data}`).join("\n\n")
+            }, {
+                type: "text",
+                text: `Dashboard Url: ${relevantData.liveboard}`,
+            }],
+        };
+    }
+
+}
 
 export default new OAuthProvider({
     apiHandlers: {
