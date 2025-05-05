d11c1a57d0f1d33ce67e60c6df3b2c512b327fe4
Shikhar Bhargava <shikhar.bhargava@thoughtspot.com>
2025-05-05 13:18:36 -0700

    Handle GET /authorize endpoint

--- src/handlers.ts
+++ src/handlers.ts
@@ -1,6 +1,7 @@
 import type { AuthRequest, OAuthHelpers } from '@cloudflare/workers-oauth-provider'
 import { Hono } from 'hono'
 import { Props } from './utils';
+import { parseRedirectApproval, renderApprovalDialog, renderInfoDialog } from './oauth-manager/oauth-utils';
 
 
 const app = new Hono<{ Bindings: Env & { OAUTH_PROVIDER: OAuthHelpers } }>()
@@ -12,28 +13,52 @@ app.get("/", async (c) => {
 });
 
 app.get("/authorize", async (c) => {
-    // TODO: Implement
-    console.log("Authorizing");
-    const oauthReqInfo = await c.env.OAUTH_PROVIDER.parseAuthRequest(c.req.raw)
+    const oauthReqInfo = await c.env.OAUTH_PROVIDER.parseAuthRequest(c.req.raw);
+    const { clientId } = oauthReqInfo
+	if (!clientId) {
+		return c.text('Invalid request', 400)
+	}
+    return renderApprovalDialog(c.req.raw, {
+		client: await c.env.OAUTH_PROVIDER.lookupClient(clientId),
+		server: {
+			name: "ThoughtSpot MCP Server",
+			logo: "https://avatars.githubusercontent.com/u/8906680?s=200&v=4",
+			// description: 'MCP Server for ThoughtSpot Agent', // optional
+		},
+		state: { oauthReqInfo }, // arbitrary data that flows through the form submission below
+	})
+})
+
+app.post("/authorize", async (c) => {
+
+    // Validates form submission, extracts state, and generates Set-Cookie headers to skip approval dialog next time
+	const { state, instanceUrl } = await parseRedirectApproval(c.req.raw)
+	if (!state.oauthReqInfo) {
+		return c.text('Invalid request', 400)
+	}
+
+    if (!instanceUrl) {
+        return new Response('Missing instance URL', { status: 400 });
+    }
+
+    const oauthReqInfo = state.oauthReqInfo;
+
+    console.log("instanceUrl", instanceUrl);
+    // Complete the authorization with the provided information
     const { redirectTo } = await c.env.OAUTH_PROVIDER.completeAuthorization({
         request: oauthReqInfo,
-        userId: "userId",
+        userId: "default", // Using a default user ID since username is not required
         metadata: {
-            label: "username",
+            label: "default",
         },
         scope: oauthReqInfo.scope,
-        // This will be available on this.props inside ThoughtSpotMCP
         props: {
             accessToken: await c.env.accessToken.get(),
-            instanceUrl: await c.env.instanceUrl.get(),
+            instanceUrl: instanceUrl,
         } as Props,
     });
 
     return Response.redirect(redirectTo);
-})
-
-app.post("/authorize", async (c) => {
-    // TODO: Implement
 })
 
 app.get("/callback", async (c) => {
