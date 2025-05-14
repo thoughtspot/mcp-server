6e42e0d723a2f10000b29eca692ed1bbcf32f6cb
Shikhar Bhargava <shikhar.bhargava@thoughtspot.com>
2025-05-14 14:41:27 -0700

    Added api to create openapi spec creation from zod schema

--- src/utils.ts
+++ src/utils.ts
@@ -2,3 +2,8 @@ export type Props = {
     accessToken: string;
     instanceUrl: string;
 };
+
+export const capitalize = (s: string): string => {
+    if (!s) return '';
+    return s.charAt(0).toUpperCase() + s.slice(1);
+};
