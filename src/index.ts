f706eb1aec5489896d73c7021ef2286139bc23c5
Shikhar Bhargava <shikhar.bhargava@thoughtspot.com>
2025-06-24 15:03:51 -0700

    integrate otel with honeycomb worker

--- src/index.ts
+++ src/index.ts
@@ -5,23 +5,26 @@ import type { Props } from "./utils";
 import { MCPServer } from "./servers/mcp-server";
 import { apiServer } from "./servers/api-server";
 import { withBearerHandler } from "./bearer";
+import { InstrumentedOAuthProviderDO, ThoughtSpotMCP } from "./durable-objects/otel-oauth-provider";
+import { trace } from "@opentelemetry/api";
 
-export class ThoughtSpotMCP extends McpAgent<Env, any, Props> {
-    server = new MCPServer(this);
+// export class ThoughtSpotMCP extends McpAgent<Env, any, Props> {
+//     server = new MCPServer(this);
 
-    async init() {
-        await this.server.init();
+//     async init() {
+//         await this.server.init();
+//     }
+// }
+
+// Export the instrumented durable objects for Wrangler
+export { InstrumentedOAuthProviderDO, ThoughtSpotMCP };
+
+// Create a simple handler that delegates to the durable object
+export default {
+    async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+        // Get or create the durable object instance
+        const id = env.OAUTH_PROVIDER_DO.idFromName('oauth-provider');
+        const obj = env.OAUTH_PROVIDER_DO.get(id);
+        return obj.fetch(request);
     }
-}
-
-export default new OAuthProvider({
-    apiHandlers: {
-        "/mcp": ThoughtSpotMCP.serve("/mcp") as any, // TODO: Remove 'any'
-        "/sse": ThoughtSpotMCP.serveSSE("/sse") as any, // TODO: Remove 'any'
-        "/api": apiServer as any, // TODO: Remove 'any'
-    },
-    defaultHandler: withBearerHandler(handler, ThoughtSpotMCP) as any, // TODO: Remove 'any'
-    authorizeEndpoint: "/authorize",
-    tokenEndpoint: "/token",
-    clientRegistrationEndpoint: "/register",
-});
+};
