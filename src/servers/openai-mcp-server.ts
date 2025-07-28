4f50566549bdad860ef5cd55db22fba28bcafed2
Shikhar Bhargava <shikhar.bhargava@thoughtspot.com>
2025-07-28 17:41:15 -0400

    use a comprehensive prompt to fetch image

--- src/servers/openai-mcp-server.ts
+++ src/servers/openai-mcp-server.ts
@@ -152,7 +152,23 @@ export class OpenAIDeepResearchMCPServer extends BaseMCPServer {
             await putInKV(token, tokenData, this.ctx.env);
             tokenUrl = `${this.ctx.env?.HOST_NAME}/data/img?token=${token}`;
         }
-        const content = `The data associated with the question is: ${answer.data} \n Use this URL : \"${tokenUrl}\" to GET the charts associated with the data. Use this PNG image of the visualization to do a graphical analysis of the data and present this graph as part of the analysis to the user. Show the graph to the user in the response.`;
+        const content = `The data associated with the question is: ${answer.data}
+
+IMPORTANT: You must fetch and display the visualization chart for this data:
+1. Make an HTTP GET request to this URL: ${tokenUrl}
+2. Use these headers for the request:
+   - Accept: image/png, image/*, */*
+3. The response will be a PNG image containing a data visualization chart
+4. Download the image and display it to the user as part of your response
+5. You can use this chart image to provide a comprehensive graphical analysis of the data as well
+
+URL to fetch the chart image: ${tokenUrl}
+
+Example request:
+GET ${tokenUrl}
+Accept: image/png, image/*, */*
+
+Please fetch this image now and show it to the user along with your analysis of both the data and the visualization.`;
 
         const result = {
             id,
