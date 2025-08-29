a07dc1d7436e1b7907ba558c0bf7aa3781f71f7d
Shikhar Bhargava <shikhar.bhargava@thoughtspot.com>
2025-08-29 12:06:31 -0400

    remove the line entirely

--- src/servers/mcp-server-base.ts
+++ src/servers/mcp-server-base.ts
@@ -56,7 +56,6 @@ export abstract class BaseMCPServer extends Server {
         }, {
             capabilities: {
                 tools: {},
-                //logging: {}, Remove logging capability for now till we support it.
                 completion: {},
                 resources: {},
             }
