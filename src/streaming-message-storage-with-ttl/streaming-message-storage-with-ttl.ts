1a1dffb7857f61f2f6bf3e5771c80860bc506b46
Rifdhan Nazeer <rifdhan.nazeer@thoughtspot.com>
2026-03-24 16:43:04 -0700

    Create class to handle streaming messages state
    - Ability to store and retrieve message state, with TTL
    - Bookmark last seen message, so that we can return only the new
      messages after that point
    - Design is resilient to new data arriving while old data is being
      fetched and bookmarked

---
+++ src/streaming-message-storage-with-ttl/streaming-message-storage-with-ttl.ts
@@ -0,0 +1,66 @@
+import type { StreamingMessagesState } from "../thoughtspot/types";
+
+const DEFAULT_TTL_SECONDS = 30 * 60; // 30 minutes
+
+type StreamingMessagesWithTtlState = StreamingMessagesState & {
+	timerId: string;
+};
+
+export class StreamingMessagesStorageWithTtl {
+	constructor(
+		private storage: DurableObjectStorage,
+		private scheduleTimer: (
+			delaySeconds: number,
+			conversationId: string,
+		) => Promise<string>,
+		private cancelTimer: (timerId: string) => Promise<void>,
+		private ttlSeconds = DEFAULT_TTL_SECONDS,
+	) {}
+
+	public async appendMessagesAndRestartTtl(
+		conversationId: string,
+		newMessages: string[],
+		isDone: boolean,
+	): Promise<void> {
+		const oldState: StreamingMessagesWithTtlState | undefined =
+			await this.storage.get(conversationId);
+		if (oldState?.timerId) {
+			await this.cancelTimer(oldState.timerId);
+		}
+
+		const timerId = await this.scheduleTimer(this.ttlSeconds, conversationId);
+		await this.storage.put(conversationId, {
+			messages: [...(oldState?.messages ?? []), ...newMessages],
+			isDone,
+			timerId,
+		});
+	}
+
+	public async getNewMessagesAndUpdateBookmark(
+		conversationId: string,
+	): Promise<StreamingMessagesState> {
+		const bookmark: number =
+			(await this.storage.get(`${conversationId}-bookmark`)) ?? 0;
+
+		const streamingMessagesState: StreamingMessagesWithTtlState | undefined =
+			await this.storage.get(conversationId);
+		if (!streamingMessagesState) {
+			throw new Error("State not found");
+		}
+
+		await this.storage.put(
+			`${conversationId}-bookmark`,
+			streamingMessagesState.messages.length,
+		);
+
+		const newMessages = streamingMessagesState.messages.slice(bookmark);
+		return {
+			messages: newMessages,
+			isDone: streamingMessagesState.isDone,
+		};
+	}
+
+	public async onTimerTriggered(conversationId: string): Promise<void> {
+		await this.storage.delete([conversationId, `${conversationId}-bookmark`]);
+	}
+}
